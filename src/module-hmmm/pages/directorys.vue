37dfa77b7b4d7bfb653f2316c7822c89f6de30f1
Jiao Jian <903939365@qq.com>
2020-01-03 10:20:24 +0800

    筛选区域及新增按钮--静态展示

--- src/module-hmmm/pages/directorys.vue
+++ src/module-hmmm/pages/directorys.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="dashboard-container">
-    <div class="app-container">目录管理132</div>
+    <div class="app-container">
+      <el-button type="primary" class="bgcolor">
+        <i class="el-icon-plus marginRight"></i>新增目录
+      </el-button>
+      <el-card style="margin-top:20px">
+        <span class="marginRight">目录名称</span>
+        <el-select placeholder="请选择"  class="marginRight"></el-select>
+        <span class="marginRight marginLeft">状态</span>
+        <el-select class="marginRight" placeholder="请选择"></el-select>
+        <el-button class="marginLeft">清除</el-button>
+        <el-button class="bgcolor" style="color:#fff">搜索</el-button>
+      </el-card>
+    </div>
   </div>
 </template>
 
@@ -14,4 +26,13 @@ export default {
 </script>
 
 <style scoped>
+.bgcolor{
+  background: linear-gradient(to right, #1493fa, #01c6fa)
+}
+.marginRight{
+  margin-right: 10px;
+}
+.marginLeft{
+  margin-left: 10px;
+}
 </style>
